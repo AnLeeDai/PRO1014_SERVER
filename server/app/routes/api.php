1f62a42ac73c678aa6aed6928df41c68cb787e48
An Le Dai <55825127+AnLeeDai@users.noreply.github.com>
2025-03-20 02:13:27 +0700

    bugfix: be fix undefind login api (#13)

--- server/app/routes/api.php
+++ server/app/routes/api.php
@@ -8,31 +8,31 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
-    http_response_code(200);
-    exit();
+  http_response_code(200);
+  exit();
 }
 
 $request = $_GET['request'] ?? '';
 $method = $_SERVER['REQUEST_METHOD'];
 
 $routes = [
-    // authentication routes
-    "post-register" => ["POST" => "AuthController@handleRegister"],
-    "post-login" => ["POST" => "AuthController@handleLogin"],
-    "post-change-password" => ["POST" => "AuthController@handleChangePassword"],
-    "post-forgot-password" => ["POST" => "AuthController@handleForgotPassword"],
+  // authentication routes
+  "post-register" => ["POST" => "AuthController@handleRegister"],
+  "post-login" => ["POST" => "AuthController@handleLogin"],
+  "post-change-password" => ["POST" => "AuthController@handleChangePassword"],
+  "post-forgot-password" => ["POST" => "AuthController@handleForgotPassword"],
 
-    // user routes
-    // GET http://localhost/your_project/api/users?page=2
+  // user routes
+  // GET http://localhost/your_project/api/users?page=2
 
-    "get-user" => ["GET" => "UserController@handleGetAllUser"],
+  "get-user" => ["GET" => "UserController@handleGetAllUser"],
 ];
 
 if (isset($routes[$request][$method])) {
-    list($controllerName, $methodName) = explode("@", $routes[$request][$method]);
-    $controller = new $controllerName();
-    $controller->$methodName();
+  list($controllerName, $methodName) = explode("@", $routes[$request][$method]);
+  $controller = new $controllerName();
+  $controller->$methodName();
 } else {
-    http_response_code(400);
-    echo json_encode(["error" => "Invalid API request"]);
+  http_response_code(400);
+  echo json_encode(["error" => "Invalid API request"]);
 }
