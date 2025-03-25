1ab8f47d4e7de1866aaa315e43c56367d29a788e
Le Dai An <ledaian22@gmail.com>
2025-03-26 00:45:57 +0700

    hotfix: allowed domain

--- server/helper/cors.php
+++ server/helper/cors.php
@@ -1,6 +1,16 @@
 <?php
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Origin: http://localhost:5173");
+$allowed_origins = [
+  "http://localhost:5173",
+  "https://pro-1014-client.vercel.app"
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? "";
+
+if (in_array($origin, $allowed_origins)) {
+  header("Access-Control-Allow-Origin: $origin");
+  header("Access-Control-Allow-Credentials: true");
+}
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
